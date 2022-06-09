f7160ee7a75d2afda3fbeddc5bc390be641d0a66
sott120 <psy_sott120@naver.com>
2022-06-09 17:49:37 +0900

    스크롤 애니메이션 구현

--- js/sy_script.js
+++ js/sy_script.js
@@ -30,3 +30,21 @@ var swiper = new Swiper('.his_swiper', {
         },
     },
 });
+
+// 스크롤 애니메이션
+
+const tarnsStartMg = 100;
+const transList = document.querySelectorAll('.int_transition');
+
+const animation = function () {
+    for (const element of transList) {
+        if (!element.classList.contains('show')) {
+            if (window.innerHeight > element.getBoundingClientRect().top) {
+                element.classList.add('show');
+            }
+        }
+    }
+};
+
+window.addEventListener('load', animation);
+window.addEventListener('scroll', animation);
