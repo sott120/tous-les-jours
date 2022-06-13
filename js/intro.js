c0d7c2e0f9440fff8f6c900cf319dcee408cde0d
sott120 <psy_sott120@naver.com>
2022-06-13 18:19:08 +0900

    파일 분리, intro scrollNav추가중

--- js/intro.js
+++ js/intro.js
@@ -32,7 +32,7 @@ let swiper = new Swiper('.his_swiper', {
     },
 });
 
-//인트로 스크롤 애니메이션
+//인트로 페이지 스크롤시 애니메이션 실행
 
 const transList = document.querySelectorAll('.int_transition');
 
@@ -53,3 +53,51 @@ const animation = function () {
 };
 
 window.addEventListener('scroll', animation);
+
+// 클릭시 페이지 스크롤링
+// document.querySelector('.scrollnav_wrap').addEventListener('click', (e) => {
+//     console.log('클릭');
+//     if (e.target.nodeName === 'LI') {
+//         let id_value = e.target.dataset.link;
+//         console.log(id_value);
+//         document.querySelector(id_value).scrollIntoView({ behavior: 'smooth' });
+//     }
+// });
+
+// if (window.scrollY > 500) {
+//     //스크롤 위치 500 높을시
+//     //상단 이동 버튼 생성(토마토색)
+//     document.querySelector('.arrow-up').classList.add('visible');
+//     //하단 이동 버튼 삭제(초록색)
+//     document.querySelector('.arrow-down').classList.remove('visible');
+// } else {
+//     //스크롤 위치 500 아래일시
+//     //상단 이동 버튼 삭제(토마토색)
+//     document.querySelector('.arrow-up').classList.remove('visible');
+//     //하단 이동 버튼 생성(초록색)
+//     document.querySelector('.arrow-down').classList.add('visible');
+// }
+
+// //오렌지색 버튼 클릭시, 최상단 태그로 이동
+// document.querySelector('.arrow-up').addEventListener('click', (e) => {
+//     document.querySelector('.container').scrollIntoView({
+//         behavior: 'smooth',
+//     });
+// });
+// //초록색 버튼 클릭시, 최하단 태그로 이동
+// document.querySelector('.arrow-down').addEventListener('click', (e) => {
+//     document.querySelector('.container2').scrollIntoView({
+//         behavior: 'smooth',
+//     });
+// });
+
+let scrollList = document.querySelectorAll('.scrollnav_circle');
+
+function navChange() {
+    console.log(window.scrollY);
+    if (window.scrollY > 700) {
+        scrollList[0].style.backgroundColor = '#999';
+    }
+}
+
+window.addEventListener('scroll', navChange);
